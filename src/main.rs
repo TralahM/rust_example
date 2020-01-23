6a0d8db0e24b84500c7552ffc7ec27e4c046c130
TralahM <musyoki.tralah@students.jkuat.ac.ke>
2020-01-23 03:04:26 +0300

    modularity, advanced formating,debug strings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod module1;
 fn do_sth(){
     // C style comments
     let x=90;
@@ -14,10 +15,14 @@ fn do_sth(){
     println!("Doing sth with arrays");
     let arry=[1,3,4,5,6,7];
     println!("{}",arry[3]);
+    println!("{:?}",arry);
     println!("Doing sth with tuples unpacking");
     let tupl=("hello",23.2,100);
     let (t1,t2,t3)=tupl; //unpacking
-    println!("{}\n {}\n {}\n",t1,t2,t3);
+    println!("{0}\n {1}\n {2}\n",t1,t2,t3);
+    println!("{v1}\n {v2}\n {v3}\n",v2=t1,v1=t2,v3=t3);
+    println!("{} to Types bin: {:b}, oct: {:o} hex: {:x}",10,10,10,10);
+    println!("Variable debug for tupl is {:?}",tupl);
     println!("Doing sth with loops over arrays");
     for i in arry.iter(){
         println!("{}",i);
@@ -28,6 +33,8 @@ fn main() {
     println!("Hello, world!");
     do_sth();
     fx_args("Tralah");
+    println!("Using modules...");
+    module1::run();
 }
 
 fn fx_args(name: &str){
